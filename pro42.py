25d7805310ed9471b2f6cc8eada4b65b8262bdb2
Shaiktasleem <41770239+Shaiktasleem@users.noreply.github.com>
2018-09-08 10:30:51 +0530

    Create pro42.py

---
+++ pro42.py
@@ -0,0 +1,14 @@
+x1,x2=map(str,raw_input().split())
+l1=len(x1)
+l2=len(x2)
+if(l1=l2):
+	if(x1=x2):
+		print(x1)
+	elif(x1>x2):
+		print(x1)
+	else:
+		print(x2)
+elif(l1>l2):
+	print x1
+else:
+	print x2
